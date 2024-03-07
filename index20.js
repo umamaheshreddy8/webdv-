d34c2fe70781d10406a001064d8fe4f02f1c7827
umamaheshreddy8 <153646414+umamaheshreddy8@users.noreply.github.com>
2024-03-07 17:34:02 +0530

    Add files via upload

---
+++ index20.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form");
+const latitudeInput=document.getElementById("latitude");
+const latitudeInput=document.getElementById("longitude");
+const result=documnt.getElementById("result");
+const aqiresult=documnt.getElementById("aqi");
+const coresult=documnt.getElementById("co");
+const no2result=documnt.getElementById("no2");
+const o3result=documnt.getElementById("o3");
+const pm2result=documnt.getElementById("pm2.5");
+const pm10result=documnt.getElementById("pm10");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const latitute =latitudeInput
+
+});
